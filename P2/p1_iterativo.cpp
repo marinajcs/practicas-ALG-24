bc09d9e2b139e9eec2a951cc182ca605f585d0f4
rafajm02 <101177060+rafajm02@users.noreply.github.com>
2024-04-01 18:33:05 +0200

    problema mayoria absoluta

---
+++ P2/p1_iterativo.cpp
@@ -0,0 +1,84 @@
+#include <cstdlib> // Para usar srand y rand
+#include <chrono>
+#include <cstring> // Para usar memcpy
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <fstream> // Para usar ficheros
+using namespace std;
+
+int encuentraMayoria(const vector<int>& nums) {
+    int n = nums.size();
+    for (int i = 0; i < n; i++) {
+        int count = 0;
+        for (int j = 0; j < n; j++) {
+            if (nums[i] == nums[j]) {
+                count++;
+            }
+        }
+        if (count > n / 2) {
+            return nums[i];
+        }
+    }
+    return -1; // No hay un elemento mayoritario
+}
+
+
+int main(int argc, char *argv[]) {
+	
+	int n, i, argumento;
+    chrono::time_point<std::chrono::high_resolution_clock> t0, tf; // Para medir el tiempo de ejecución
+	double tejecucion; // tiempo de ejecucion del algoritmo en ms
+	unsigned long int semilla;
+	ofstream fsalida;
+	
+	if (argc <= 3) {
+		cerr<<"\nError: El programa se debe ejecutar de la siguiente forma.\n\n";
+		cerr<<argv[0]<<" NombreFicheroSalida Semilla tamCaso1 tamCaso2 ... tamCasoN\n\n";
+		return 0;
+	}
+	
+	// Abrimos fichero de salida
+	fsalida.open(argv[1]);
+	if (!fsalida.is_open()) {
+		cerr<<"Error: No se pudo abrir fichero para escritura "<<argv[1]<<"\n\n";
+		return 0;
+	}
+
+    vector<int> V;
+	
+	// Inicializamos generador de no. aleatorios
+	semilla= atoi(argv[2]);
+	srand(semilla);
+	
+	// Pasamos por cada tamaÒo de caso
+	for (argumento = 2; argumento<argc; argumento++) {
+		
+		// Cogemos el tamanio del caso
+		n= atoi(argv[argumento]);
+		
+		// Reservamos memoria para el vector
+		V.resize(n);
+
+		// Generamos vector aleatorio de prueba, con componentes entre 0 y n-1
+		for (i= 0; i<n; i++)
+			V[i]= rand()%n;
+		
+		t0= std::chrono::high_resolution_clock::now(); // Cogemos el tiempo en que comienza la ejecuciÛn del algoritmo
+		int mayoria = encuentraMayoria(V); // Ejecutamos el algoritmo para tamaÒo de caso n
+		tf= std::chrono::high_resolution_clock::now(); // Cogemos el tiempo en que finaliza la ejecuciÛn del algoritmo
+		
+		unsigned long tejecucion= std::chrono::duration_cast<std::chrono::microseconds>(tf - t0).count();
+		
+		cerr << "n: "<< n << "\t\tTiempo (us): " << tejecucion <<endl;
+		
+		// Guardamos tam. de caso y t_ejecucion a fichero de salida
+		fsalida<<n<<" "<<tejecucion<<"\n";
+		
+	}
+	
+	// Cerramos fichero de salida
+	fsalida.close();
+	
+	return 0;
+}
